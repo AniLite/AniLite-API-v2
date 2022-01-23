3f547bdf5f6504ffce0af28c38f2023cb18707ac
Shivansh <shuklashivansh26@gmail.com>
2022-01-24 00:53:06 +0530

    Removed some test stuff, updated gitignore

--- anilite_v2/urls.py
+++ anilite_v2/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from animu.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('animu.api.urls')),
     path('user/', include('users.urls')),
-    path('test', test)
 ]
--- animu/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .tasks import test_func
-
-
-def test(request):
-    test_func.delay()
-    return HttpResponse('Done')
